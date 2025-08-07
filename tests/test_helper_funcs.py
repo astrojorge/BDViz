ba1a0e87bb1bef98236dab832a1c4b45eddda07e
bkotten <bkotten@umich.edu>
2025-08-07 15:42:45 -0400

    added more thoughts to ang sep

--- tests/test_helper_funcs.py
+++ tests/test_helper_funcs.py
@@ -6,6 +6,24 @@ def test_ang_sep():
     bd2 = BrownDwarf("BD-1", ra=150, dec=25, distance=5, color='purple')
     get_angular_separation(bd1, bd1)
 
+# def test_ang_sep_known_value():
+#     # Brown dwarfs with 5 degrees separation in declination
+#     bd1 = BrownDwarf("BD-1", ra=150, dec=20, distance=5, color='purple')
+#     bd2 = BrownDwarf("BD-2", ra=150, dec=25, distance=5, color='purple')
+
+#     result = get_angular_separation(bd1, bd2)
+
+#     # Assert it's an angle in degrees
+#     assert result.unit == u.deg
+
+#     # Because RA is the same, angular separation = |dec1 - dec2| = 5 deg
+#     assert abs(result.value - 5.0) < 1e-5
+
+# def test_ang_sep_zero():
+#     bd1 = BrownDwarf("BD-1", ra=150, dec=20, distance=5, color='purple')
+#     result = get_angular_separation(bd1, bd1)
+
+#     assert result.value == 0
 
 # if __name__ == "__main__":
 #     # test_period_basis()
